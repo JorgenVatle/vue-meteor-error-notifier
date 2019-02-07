79782a8552fd932968d0886dd62aab08247f9828
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-07 17:01:38 +0200

    Extend Vue with $notifyError() utility on install.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Vue } from 'vue/types/vue';
-import { NotificationInterfaces } from './interfaces';
+import { MeteorError, NotificationInterfaces } from './interfaces';
+import ErrorNotifier from './ErrorNotifier';
 
 interface VueMeteorErrorNotificationOptions {
     notifier: NotificationInterfaces;
 }
 
 export default {
-    install(Vue: Vue, { notifier = 'native' }: VueMeteorErrorNotificationOptions) {
+    install(vue: Vue, { notifier = 'native' }: VueMeteorErrorNotificationOptions) {
         if (notifier === 'native') {
             console.warn(
                 'You\'re running VueMeteorErrorNotifier in `native` mode.\n' +
@@ -14,6 +15,10 @@ export default {
                 'Use an interface like `buefy` or `sweetalert2` for non-blocking notifications that look great.\n\n' +
                 'See https://github.com/JorgenVatle/vue-meteor-error-notifier#notification-interfaces for more information.'
             )
+        }
+
+        vue.prototype.$notifyError = function (error: MeteorError) {
+            new ErrorNotifier(error, vue)[notifier]();
         }
     }
 }
