834964b3c9c2c3f42523281d8ed46979de1988fd
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-07 17:19:31 +0200

    Add line break to native warning message on install.

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ export default {
         if (notifier === 'native') {
             console.warn(
                 'You\'re running VueMeteorErrorNotifier in `native` mode.\n' +
-                'While this is the default interface, it is not the best for the user experience.' +
+                'While this is the default interface, it is not the best for the user experience.\n' +
                 'Use an interface like `buefy` or `sweetalert2` for non-blocking notifications that look great.\n\n' +
                 'See https://github.com/JorgenVatle/vue-meteor-error-notifier#notification-interfaces for more information.'
             )
