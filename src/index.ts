53db146f35d69ff5d93e7c121a8aa57fd556ca4a
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-07 19:34:46 +0200

    Add warning message for non-available notification interface.

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,25 @@ interface VueMeteorErrorNotificationOptions {
     notifier: NotificationInterfaces;
 }
 
+const warn = (message) => {
+    console.warn('[VueMeteorErrorNotifier]', message);
+};
+
 export default {
     install(vue: typeof Vue, { notifier = 'native' }: VueMeteorErrorNotificationOptions) {
         if (notifier === 'native') {
-            console.warn(
+            warn(
                 'You\'re running VueMeteorErrorNotifier in `native` mode.\n' +
                 'While this is the default interface, it is not the best for the user experience.\n' +
                 'Use an interface like `buefy` or `sweetalert2` for non-blocking notifications that look great.\n\n' +
+                'See https://github.com/JorgenVatle/vue-meteor-error-notifier#notification-interfaces for more information.'
+            )
+        }
+
+        if (!ErrorNotifier.isAvailable(vue, notifier)) {
+            warn(
+                'The notification interface you specified is not available, or isn\'t being included before\n' +
+                'VueMeteorErrorNotifier.\n\n' +
                 'See https://github.com/JorgenVatle/vue-meteor-error-notifier#notification-interfaces for more information.'
             )
         }
