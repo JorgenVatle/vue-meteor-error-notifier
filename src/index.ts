0c3f25f2c35770a40288d8e980f4a91b9336c69c
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-07 14:25:13 +0200

    Add warning message for `native` notification interface.

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,13 @@ interface VueMeteorErrorNotificationOptions {
 
 export default {
     install(Vue: Vue, { notifier = 'native' }: VueMeteorErrorNotificationOptions) {
-
+        if (notifier === 'native') {
+            console.warn(
+                'You\'re running VueMeteorErrorNotifier in `native` mode.\n' +
+                'While this is the default interface, it is not the best for the user experience.' +
+                'Use an interface like `buefy` or `sweetalert2` for non-blocking notifications that look great.\n\n' +
+                'See https://github.com/JorgenVatle/vue-meteor-error-notifier#notification-interfaces for more information.'
+            )
+        }
     }
 }
